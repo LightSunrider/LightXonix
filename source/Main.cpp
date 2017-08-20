18fe966c1c83e2231287cb461522c75555597556
Vladislav Glebovich <v@lrd.by>
2017-08-20 22:11:59 +0300

    Camera usage

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,13 +1,32 @@
 #include "Engine/Engine.hpp"
-#include <ctime>
-#include <iostream>
 #include <GLFW/glfw3.h>
+#include <ctime>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+
+// Function prototypes
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void Do_Movement();
+
+// Camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+bool keys[1024];
+GLfloat lastX = 400, lastY = 300;
+bool firstMouse = true;
+
+GLfloat deltaTime = 0.0f;
+GLfloat lastFrame = 0.0f;
 
 signed main() {
     GameWindow window = GameWindow(800, 600, "LightXonix");
+    glfwSetKeyCallback(window.m_GlfwWindow, key_callback);
+    glfwSetCursorPosCallback(window.m_GlfwWindow, mouse_callback);
+    glfwSetScrollCallback(window.m_GlfwWindow, scroll_callback);
+    glfwSetInputMode(window.m_GlfwWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     Shader simpleShader = Shader("Shaders/simple.vert", "Shaders/simple.frag");
     Texture simpleTexture = Texture("Textures/simple.dds");
@@ -71,24 +90,24 @@ signed main() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid *) 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*) 0);
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid *) (3 * sizeof(GLfloat)));
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*) (3 * sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
 
     glBindVertexArray(0);
 
-    long lastFrame = clock(), currentFrame;
-    float delta = 0.0f;
-
     window.MakeContextCurrent();
     while (!window.ShouldClose()) {
-        currentFrame = clock();
-        delta = (float) (currentFrame - lastFrame) / 1000;
+        GLfloat currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        Do_Movement();
 
         glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -99,10 +118,11 @@ signed main() {
         glBindTexture(GL_TEXTURE_2D, simpleTexture.Id);
         glUniform1i(glGetUniformLocation(simpleShader.Id, "Texture"), 0);
 
+        // Create camera transformation
         glm::mat4 view;
+        view = camera.GetViewMatrix();
         glm::mat4 projection;
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-        projection = glm::perspective(45.0f, (GLfloat)800 / (GLfloat)600, 0.1f, 100.0f);
+        projection = glm::perspective(camera.Zoom, (float) 800 / (float) 600, 0.1f, 1000.0f);
 
         GLint modelLoc = glGetUniformLocation(simpleShader.Id, "model");
         GLint viewLoc = glGetUniformLocation(simpleShader.Id, "view");
@@ -116,7 +136,7 @@ signed main() {
 
         glm::mat4 model;
         model = glm::translate(view, glm::vec3(0.0f, 0.0f, 0.0f));
-        model = glm::rotate(model, (GLfloat)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
+        model = glm::rotate(model, (GLfloat) glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
         model = glm::scale(model, glm::vec3(2.5f, 2.5f, 2.5f));
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
@@ -132,4 +152,52 @@ signed main() {
     window.Close();
 
     return 0;
+}
+
+// Moves/alters the camera positions based on user input
+void Do_Movement() {
+    std::cout << "5051";
+    // Camera controls
+    if (keys[GLFW_KEY_W])
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (keys[GLFW_KEY_S])
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (keys[GLFW_KEY_A])
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (keys[GLFW_KEY_D])
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+    // cout << key << endl;
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    if (key >= 0 && key < 1024) {
+        if (action == GLFW_PRESS)
+            keys[key] = true;
+        else if (action == GLFW_RELEASE)
+            keys[key] = false;
+    }
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+    if (firstMouse) {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    GLfloat xoffset = xpos - lastX;
+    GLfloat yoffset = lastY - ypos;// Reversed since y-coordinates go from bottom to left
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+    camera.ProcessMouseScroll(yoffset);
 }
