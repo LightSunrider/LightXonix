9458d2d6740749e32ea9d8b2b4d3529f7539cd61
Vladislav Glebovich <v@lrd.by>
2017-09-11 20:42:40 +0300

    Added GameObject class

---
+++ source/Engine/GameObject.hpp
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include <glm/glm.hpp>
+
+namespace le {
+
+class GameObject {
+public:
+    GameObject(glm::vec3 position = glm::vec3(), glm::vec3 rotation = glm::vec3(), glm::vec3 scale = glm::vec3()) {
+        this->Position = position;
+        this->Rotation = rotation;
+        this->Scale = scale;
+    }
+
+    glm::vec3 Position;
+    glm::vec3 Rotation;
+    glm::vec3 Scale; 
+
+private:
+    void OnCreate() { };
+    void OnUpdate() { };
+    void OnDestroy() { };
+};
+}
